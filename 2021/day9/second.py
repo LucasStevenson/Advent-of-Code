e8cfe18921ccb41c018012d309e845e3c5539c7f
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2021-12-31 21:52:35 -0800

    completed day9 pt2

--- 2021/day9/second.py
+++ 2021/day9/second.py
@@ -3,7 +3,6 @@ infile = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
 with open(infile) as f:
     lines = [ list(map(int, list(line.rstrip()))) for line in f.readlines() if line.strip() ]
-    print(lines)
 
 lowestPoints = []
 for r in range(len(lines)):
@@ -15,5 +14,19 @@ for r in range(len(lines)):
         if lines[r][c] < left and lines[r][c] < right and lines[r][c] < up and lines[r][c] < down:
             lowestPoints.append((r, c))
 
-print(lowestPoints)
+points = set()
 
+def solution(r, c):
+    if (r < 0 or r >= len(lines) or c < 0 or c >= len(lines[r])) or (lines[r][c] == 9) or ((r, c) in points):
+        return 0
+    points.add((r, c))
+    size = 1
+    size += solution(r, c-1) + solution(r, c+1) + solution(r+1, c) + solution(r-1, c)
+    return size
+
+
+arr = []
+for i in lowestPoints:
+    arr.append(solution(i[0], i[1]))
+arr = sorted(arr)
+print(arr[-1]*arr[-2]*arr[-3])
