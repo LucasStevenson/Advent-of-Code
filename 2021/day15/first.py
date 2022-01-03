39b373605a251cdc6149af235794de75c4ac3350
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2022-01-03 13:04:11 -0800

    used priority queue to speed up process of choosing node with smallest weight

--- 2021/day15/first.py
+++ 2021/day15/first.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 from collections import defaultdict
 
 infile = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
@@ -10,45 +11,20 @@ for line in open(infile):
 r, c = 0, 0
 distances = defaultdict(lambda: float("inf"))
 distances[(0,0)] = 0
-relaxedPoints = set()
+pq = [(0, (r, c))]
 
-
-def updateDistances(r, c):
-    if c + 1 < len(lines[0]):
-        down = (lines[r][c+1], (r, c+1))
-        if distances[(r, c)] + down[0] < distances[down[1]]:
-            distances[down[1]] = distances[(r, c)] + down[0]
-
-    if r + 1 < len(lines):
-        right = (lines[r+1][c], (r+1, c))
-        if distances[(r, c)] + right[0] < distances[right[1]]:
-            distances[right[1]] = distances[(r, c)] + right[0]
-
-    if r - 1 >= 0:
-        left = (lines[r-1][c], (r-1, c))
-        if distances[(r, c)] + left[0] < distances[left[1]]:
-            distances[left[1]] = distances[(r, c)] + left[0]
-
-    if c - 1 >= 0:
-        top = (lines[r][c-1], (r, c-1))
-        if distances[(r, c)] + top[0] < distances[top[1]]:
-            distances[top[1]] = distances[(r, c)] + top[0]
-
-while True:
-    updateDistances(r, c)
-    smallestVal = float("inf")
-    smallestKey = None
-
-    for k, v in distances.items():
-        if k in relaxedPoints:
+while pq:
+    for dr, dc in [(0,1), (0,-1), (1,0), (-1,0)]:
+        # check a point's neighbors
+        if not (0 <= r+dr < len(lines) and 0 <= c+dc < len(lines[r])):
             continue
-        if v < smallestVal:
-            smallestVal = v
-            smallestKey = k
-        
-    if smallestKey == None:
-        break
-    r, c = smallestKey[0], smallestKey[1]
-    relaxedPoints.add(smallestKey)
+        rr = r+dr
+        cc = c+dc
+        if distances[(r, c)] + lines[rr][cc] < distances[(rr, cc)]:
+            distances[(rr, cc)] = distances[(r, c)] + lines[rr][cc]
+            heapq.heappush(pq, (distances[(rr, cc)], (rr, cc)))
 
-print(distances[(len(lines)-1, len(lines[0])-1)])
+    smallestVal = heapq.heappop(pq)[1]
+    r, c = smallestVal[0], smallestVal[1]
+
+print(distances[(len(lines)-1,len(lines)-1)])
