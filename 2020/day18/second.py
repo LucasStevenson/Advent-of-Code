5d86990725a8a0eb0976635b309847080c047057
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2022-01-15 03:47:12 -0800

    deleted unnecessary code

--- 2020/day18/second.py
+++ 2020/day18/second.py
@@ -15,10 +15,8 @@ def solve(eq):
 def evaluate(eq):
     eq = eq.replace("(", "").replace(")", "").split()
     while "+" in eq:
-        for i in range(len(eq)):
-            if eq[i] == "+":
-                eq = eq[:i-1] + [str(int(eq[i-1])+int(eq[i+1]))] + eq[i+2:]
-                break
+        i = eq.index("+")
+        eq = eq[:i-1] + [str(int(eq[i-1])+int(eq[i+1]))] + eq[i+2:]
     nums = [ int(num) for num in eq if num.isdigit() ]
     return math.prod(nums)
 
