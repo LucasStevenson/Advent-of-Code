33510a23070d3886d3fde40005d7067b31b78c91
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2022-01-17 01:07:02 -0800

    completed day8 pt2

--- 2021/day08/second.py
+++ 2021/day08/second.py
@@ -1,38 +1,57 @@
-from collections import Counter, defaultdict
+from itertools import permutations
 import sys
 infile = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
-
-def sortEachEl(arr):
-    return list(map(lambda el: "".join(sorted(el)), arr.split()))
-
-zero = ["seg0", "seg1", "seg2", "seg4", "seg5", "seg6"]
-one = ["seg2", "seg5"]
-two = ["seg0", "seg2", "seg3", "seg4", "seg6"]
-three = ["seg0", "seg2", "seg3", "seg5", "seg6"]
-four = [""]
-five = []
-six = []
-seven = []
-eight = []
-nine = []
-
 with open(infile) as f:
     line = f.readline().rstrip()
-    vals = defaultdict(list)
+    sigPatterns = []
+    outputVals = []
     while line:
-        first, second = line.split(" | ")
-        first = sortEachEl(first)
-        second = sortEachEl(second)
-        for i in first:
-            if len(i) == 2:
-                vals[1] = list(i)
-            elif len(i) == 4:
-                vals[4] = list(i)
-            elif len(i) == 3:
-                vals[7] = list(i)
-            elif len(i) == 7:
-                vals[8] = list(i)
-
+        first, second  = line.split(" | ")
+        outputVals.append(second)
+        sigPatterns.append(first)
         line = f.readline().rstrip()
-    print(vals)
+perms = list(permutations("abcdefg"))
+
+'''
+ 0000    <--- array below is based on this 
+1    2      
+1    2      Each index in the array represents the number the element represents
+ 3333       
+4    5      For example, arr[0] is a list of all the segments that would have to 
+4    5      be turned on to represent the number 0
+ 6666 
+'''
+
+arr = [
+    [0,1,2,4,5,6],
+    [2,5],
+    [0,2,3,4,6],
+    [0,2,3,5,6],
+    [1,2,3,5],
+    [0,1,3,5,6],
+    [0,1,3,4,5,6],
+    [0,2,5],
+    [0,1,2,3,4,5,6],
+    [0,1,2,3,5,6]
+]
+
+def getConfig(pattern):
+    for perm in perms:
+        valid = True
+        for seq in pattern.split():
+            if sorted([ perm.index(c) for c in seq ]) not in arr:
+                valid = False
+                break
+        if valid:
+            return perm
+
+ans = 0
+for i in range(len(outputVals)):
+    config = getConfig(sigPatterns[i])
+    n = ""
+    for seq in outputVals[i].split():
+        n += str(arr.index(sorted([ config.index(c) for c in seq ])))
+    ans += int(n)
+
+print(ans)
